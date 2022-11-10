0f4b06f2cf1a5634c3c17936437289b98dfcb6d2
ManuelGonzalezL <gonzalezm3@ufl.edu>
2022-11-10 13:37:07 -0500

    Register

--- Assets/Scripts/PlayfabManager.cs
+++ Assets/Scripts/PlayfabManager.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayfabManager : MonoBehaviour
 {
-    //[Header("UI")]
-    //public Text messageText;
-    //public InputField emailInput;
-    //public InputField passwordInput;
-//
-    //public void RegisterButton() {
-    //    var request = new RegisterPlayFabUserRequest {
-    //        Email = emailInput.text,
-   //         Password = passwordInput.text,
-   //         RequireBothUsernameAndEmail=false,
-   //     };
-   //     PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
-  //  }
-  //   
-  //  void OnRegisterSuccess(RegisterPlayFabUserResult result) {
-  //      messageText.text = "Registered and logged in";}
+    [Header("UI")]
+    public TMP_Text messageText;
+    public TMP_InputField emailInput;
+    public TMP_InputField passwordInput;
+
+    public void RegisterButton() {
+        var request = new RegisterPlayFabUserRequest {
+            Email = emailInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail=false
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+    }
+    
+    void OnRegisterSuccess(RegisterPlayFabUserResult result) {
+        messageText.text = "Registered and logged in";}
 
 
     void Start(){
